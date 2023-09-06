b976feadc9bdca3963caad14a7952984fc6f3424
sankit <sankit.a@logically.co.uk>
2023-09-06 19:01:51 +0530

    chore: removed cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "organization-name"
+#   cloud {
+#     organization = "sankit_learning"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "learn-terraform-cloud"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#     }
+#   }
 
-  required_version = ">= 0.14.0"
-}
+#   required_version = ">= 0.14.0"
+# }
